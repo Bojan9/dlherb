5fe92637138805916012402d627da4d15b347e4a
bojan9 <>
2018-09-29 23:17:36 +0200

    go napraviv search barot i najdole vo footer email (koga go klikam se pravi border dali moze da se trgne nekako?). So navbarot imam problem ne mi raboti izgleda js (click). dali ste slobodni nedelava da imame cas jas sum prva smena.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,9 @@
+$("dropdown").on('click', function(){
+    $("dropdown-content").css('display', 'block');
+    $('dropdown').css('color', '#FEB403');
+})
 
+$("dropdownSearch").on('click', function(){
+    $("dropdown-contentSearch").css('display', 'block');
+    $('dropdownSearch').css('color', '#FEB403');
+})
